bd7ff726ebe3843c2b49d80febefc130f969e7fa
David Ngacha <davidngacha01@gmail.com>
2025-08-20 15:18:03 +0200

    Hero v1

--- app/page.js
+++ app/page.js
@@ -1,8 +1,12 @@
 import Hero from "@/components/homepage/Hero";
+import Intro from "@/components/homepage/Intro";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
    <Hero />
+   <Intro />
+   </>
   );
 }
--- components/homepage/Hero.jsx
+++ components/homepage/Hero.jsx
@@ -2,24 +2,40 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div className="max-w-[1280px] mx-auto px-4 md:px-0 py-20 h-screen flex items-center">
-      {/* Left Column */}
-      <div className="w-full md:w-1/2">
-        <h1 className="text-4xl font-bold text-red-500">
-          ARINOME VENTURES LIMITED
-        </h1>
-        <p className="mt-4 text-lg text-gray-700">
-          It is our aim to exceed your expectations through this engagement and we are committed to ensuring that our service is valuable to you today and in the future.
-        </p>
-      </div>
+    <section className='relative w-full bg-[#448AFF] h-screen overflow-hidden'>
+      <div className="max-w-[1280px] mx-auto px-4 md:px-0 py-20 flex flex-col md:flex-row items-center h-full">
+        {/* Text Content*/}
+        <div className="w-full md:w-1/2 z-10 mb-8 md:mb-0 text-center md:text-left">
+          <h1 className="text-3xl md:text-4xl font-bold text-white">
+            ARINOME VENTURES LIMITED
+          </h1>
+          <p className="mt-4 text-base md:text-lg text-white">
+            It is our aim to exceed your expectations through this engagement and we are committed to ensuring that our service is valuable to you today and in the future.
+          </p>
+        </div>
 
-      {/* Right Column */}
-      <div className="w-full md:w-1/2 flex justify-center">
-        {/* Placeholder for Illustrator image or code */}
-        <div className="bg-gray-200 w-64 h-64 flex items-center justify-center">
-          Image / Illustration
+        {/* Image Content */}
+        <div className="w-full md:w-1/2 flex justify-center z-10">
+          {/* Placeholder for Illustrator image or code */}
+          <div className="bg-gray-200 w-48 h-48 md:w-64 md:h-64 flex items-center justify-center rounded-lg shadow-lg">
+            Image / Illustration
+          </div>
         </div>
       </div>
-    </div>
+
+      {/* Curved Bottom SVG */}
+        <div className="absolute bottom-0 left-0 w-full -mb-1">
+        <svg 
+          viewBox="0 0 1200 200" 
+          className="w-full h-auto block"
+          preserveAspectRatio="none"
+        >
+          <path 
+            d="M0,200 L0,100 Q300,0 600,100 T1200,100 L1200,200 Z" 
+            fill="white"
+          />
+        </svg>
+      </div>
+    </section>
   )
 }
